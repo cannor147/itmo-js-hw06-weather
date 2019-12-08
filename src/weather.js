bbf093157b4c8998fa8e24bc8f72783c47b08bc2
evgenymarkov <evgenymarkov@yandex-team.ru>
2019-12-08 15:21:54 +0300

    Выложена задача «Осадки в виде фрикаделек»

---
+++ src/weather.js
@@ -0,0 +1,73 @@
+'use strict';
+
+global.fetch = require('node-fetch');
+
+/**
+ * @typedef {object} TripItem Город, который является частью маршрута.
+ * @property {number} geoid Идентификатор города
+ * @property {number} day Порядковое число дня маршрута
+ */
+
+class TripBuilder {
+  /**
+   * Метод, добавляющий условие наличия в маршруте
+   * указанного количества солнечных дней
+   * Согласно API Яндекс.Погоды, к солнечным дням
+   * можно приравнять следующие значения `condition`:
+   * * `clear`;
+   * * `partly-cloudy`.
+   * @param {number} daysCount количество дней
+   * @returns {object} Объект планировщика маршрута
+   */
+  sunny(daysCount) {
+    return this;
+  }
+
+  /**
+   * Метод, добавляющий условие наличия в маршруте
+   * указанного количества пасмурных дней
+   * Согласно API Яндекс.Погоды, к солнечным дням
+   * можно приравнять следующие значения `condition`:
+   * * `cloudy`;
+   * * `overcast`.
+   * @param {number} daysCount количество дней
+   * @returns {object} Объект планировщика маршрута
+   */
+  cloudy(daysCount) {
+    return this;
+  }
+
+  /**
+   * Метод, добавляющий условие максимального количества дней.
+   * @param {number} daysCount количество дней
+   * @returns {object} Объект планировщика маршрута
+   */
+  max(daysCount) {
+    return this;
+  }
+
+  /**
+   * Метод, возвращающий Promise с планируемым маршрутом.
+   * @returns {Promise<TripItem[]>} Список городов маршрута
+   */
+  build() {
+    return this;
+  }
+}
+
+/**
+ * Фабрика для получения планировщика маршрута.
+ * Принимает на вход список идентификаторов городов, а
+ * возвращает планировщик маршрута по данным городам.
+ *
+ * @param {number[]} geoids Список идентификаторов городов
+ * @returns {TripBuilder} Объект планировщика маршрута
+ * @see https://yandex.ru/dev/xml/doc/dg/reference/regions-docpage/
+ */
+function planTrip(geoids) {
+  return new TripBuilder(geoids);
+}
+
+module.exports = {
+  planTrip
+};
